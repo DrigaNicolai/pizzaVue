05902fa8a930ae802ff9b6a3be939536e774e08a
DrigaNicolai <just.whyme7@gmail.com>
2023-06-05 18:59:55 +0300

    Update App.vue

---
+++ pizza/src/layouts/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Header from "@/components/Header.vue";
+
+export default defineComponent({
+  name: "AppLayout",
+
+  components: {
+    Header
+  }
+})
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<style scoped></style>
