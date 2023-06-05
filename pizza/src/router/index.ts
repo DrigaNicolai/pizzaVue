0f178697109f13b4348aad9cdf67d0197d057b43
DrigaNicolai <just.whyme7@gmail.com>
2023-06-05 19:27:58 +0300

    Router settings

--- pizza/src/router/index.ts
+++ pizza/src/router/index.ts
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '@/views/index.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
@@ -11,17 +11,14 @@ const router = new VueRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/restaurant',
+      name: 'restaurant',
+      component: () => import('@/views/restaurant.vue')
     }
   ]
-})
+});
 
-export default router
+export default router;
