88def0a292bf17c1b7c1c4b2546988b6dd227411
DrigaNicolai <just.whyme7@gmail.com>
2023-06-05 20:26:15 +0300

    Restaurant card interface and component

---
+++ pizza/src/components/cards/Restaurant.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: "RestaurantCard",
+
+  props: {
+    restaurant: {
+      required: true,
+      type: Object,
+    }
+  },
+})
+</script>
+
+<template>
+  <RouterLink to="" class="card card-restaurant">
+    <img :src="restaurant.image" alt="image" class="card-image" />
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title"> {{ restaurant.name }} </h3>
+        <span class="card-tag tag">
+          {{ restaurant.time_of_delivery }} мин
+        </span>
+      </div>
+      <div class="card-info">
+        <div class="rating">
+          {{ restaurant.stars }}
+        </div>
+        <div class="price">
+          От {{ restaurant.price }} ₽
+        </div>
+        <div class="category">
+          {{ restaurant.kitchen }}
+        </div>
+      </div>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+.card-restaurant {
+  cursor: pointer
+}
+</style>
--- pizza/src/ts/interfaces/index.ts
+++ pizza/src/ts/interfaces/index.ts
@@ -1,1 +1,2 @@
 export type { LoginFormInterface } from "./login-form";
+export type { RestaurantCardInterface } from "./restaurant-card";
